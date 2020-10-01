456c7d6eca0e862a3a241c6cadafe2769f1883d3
zizoumunoz <zmunoz@outlook.es>
2020-09-30 23:25:49 -0700

    Added cool lesson

--- HelloWorldPlugin/src/main/java/me/zizoumunoz/helloworldplugin/HelloWorldPlugin.java
+++ HelloWorldPlugin/src/main/java/me/zizoumunoz/helloworldplugin/HelloWorldPlugin.java
@@ -4,4 +4,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class HelloWorldPlugin extends JavaPlugin {
 
+	@Override
+	public void onEnable() {
+		/*
+		 * "the only ways the player can interact with your plug-in is
+		 * through commands and events."
+		 */
+
+	}
+
 }
